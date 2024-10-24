9d681a1e21892742273719ea5d48ca37f2395740
Фролов Александр <frolov.mailbox@mail.ru>
2024-10-24 15:49:39 +0300

    Task003 is done

---
+++ Task003.py
@@ -0,0 +1,10 @@
+a = int (input())
+b = int (input ())
+c = int (input ())
+d = int (input ())
+e = int (input ())
+f = int (input ())
+if (a+b+c) == (d+e+f):
+    print ("Ticket is lucky")
+else:
+    print ("Ticket is unlucky")
